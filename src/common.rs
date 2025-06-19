5d7e7f1da0d314d267891f7a24a22bf2f7dc535a
luowenfu <luowenfu@qq.com>
2025-06-20 01:44:57 +0800

    修改替换API地址

--- src/common.rs
+++ src/common.rs
@@ -1004,7 +1004,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "https://nas.dfsss.com:21113".to_owned()
 }
 
 #[inline]
